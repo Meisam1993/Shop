0f0c256cd14e679c542ab6e6b7310de7ef93161b
Meisam1993 <140316923+Meisam1993@users.noreply.github.com>
2023-09-08 21:00:27 +0330

    activity_detail.xml Ui is designed.

---
+++ app/src/main/java/com/example/shop/feature/detail/product/DetailActivity.kt
@@ -0,0 +1,16 @@
+package com.example.shop.feature.detail.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shop.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
