76609b67f0ac3b1e9ce88c31f8357257f667e91a
Meisam1993 <140316923+Meisam1993@users.noreply.github.com>
2023-09-18 14:32:21 +0330

    fragment_login.xml & fragment_sign_up.xml is designed. activity_auth.xml fragmentContainer component is added.

--- app/src/main/java/com/example/shop/feature/auth/AuthActivity.kt
+++ app/src/main/java/com/example/shop/feature/auth/AuthActivity.kt
@@ -1,12 +1,25 @@
 package com.example.shop.feature.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shop.R
+import com.example.shop.base.BaseActivity
+import com.example.shop.databinding.ActivityAuthBinding
 
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : BaseActivity() {
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initViews()
+    }
+
+    private fun initViews() {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, LoginFragment())
+                .commit()
+        }
     }
 }
